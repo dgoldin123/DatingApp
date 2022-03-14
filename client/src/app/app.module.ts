11d9bdef89c70f475c78a0e3247037b6ca280365
yzidell <yzidell2@hotmail.com>
2022-03-14 15:53:49 -0400

    end of section  15

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -26,12 +26,13 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
 import { FileUploadModule } from 'ng2-file-upload';
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DateInputComponent } from './_forms/date-input/date-input.component';
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 
 @NgModule({
   declarations: [
     AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, MemberDetailComponent, 
     ListsComponent, MessagesComponent, TestErrorsComponent, NotFoundComponent, ServerErrorComponent, MemberCardComponent, 
-    MemberEditComponent, PhotoEditorComponent, TextInputComponent, DateInputComponent
+    MemberEditComponent, PhotoEditorComponent, TextInputComponent, DateInputComponent, MemberMessagesComponent
   ],
   imports: [
     BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule,
